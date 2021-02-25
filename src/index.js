fd920c0b77792b20bbb1ba0ff438cc941a1b4727
pinshengjuan <pinshengjuan@gmail.com>
2021-02-26 00:52:10 +0800

    load google map

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GoogleApiWrapper from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GoogleApiWrapper />
   </React.StrictMode>,
   document.getElementById('root')
 );
